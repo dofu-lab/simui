ece71c9c4b7e50adeba386fe6860c34b30f16201
dongphuong0905 <phuongtransmile99@gmail.com>
2025-04-15 13:19:39 +0700

    feat: change repo link

--- src/app/core/constants.ts
+++ src/app/core/constants.ts
@@ -1,3 +1,3 @@
-export const REPO_LINK = 'https://github.com/dongphuong0905/simui';
+export const REPO_LINK = 'https://github.com/dofu-lab/simui';
 export const X_LINK = 'https://x.com/PhuongTranDong';
-export const DISCUSSION_LINK = 'https://github.com/dongphuong0905/simui';
+export const DISCUSSION_LINK = 'https://github.com/dofu-lab/simui/discussions/categories/ideas';
